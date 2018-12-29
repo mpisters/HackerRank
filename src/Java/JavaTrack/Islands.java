8e75364e5a788236ed43cdf45da766553759aefe
michelle <pistersmichelle@gmail.com>
2018-12-29 21:18:42 +0100

    fix islands

--- src/Java/JavaTrack/Islands.java
+++ src/Java/JavaTrack/Islands.java
@@ -8,10 +8,10 @@
         }
 
         private void DFS(int graph[][], int rowIndex, int colIndex, boolean visited[][], int current, int ROW, int COL) {
-            int rowNbr[] = new int[]{-1, -1, -1, 0, 0, 1, 1, 1};
-            int colNbr[] = new int[]{-1, 0, 1, -1, 1, -1, 0, 1};
+            int rowNbr[] = new int[]{-1, 1,0,0,};
+            int colNbr[] = new int[]{0, 0, 1, -1};
             visited[rowIndex][colIndex] = true;
-            for (int neighb = 0; neighb < 8; ++neighb) {
+            for (int neighb = 0; neighb < 4; ++neighb) {
                 if (isSafe(graph, rowIndex + rowNbr[neighb], colIndex + colNbr[neighb], visited, ROW, COL, current)) {
 
                     DFS(graph, rowIndex + rowNbr[neighb], colIndex + colNbr[neighb], visited, current, ROW, COL);
