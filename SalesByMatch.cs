4b2dd5808b2c956eac307cdb1ffbb7cc174e6f83
mpisters <pistersmichelle@gmail.com>
2020-09-21 21:15:26 +0200

    Create SalesByMatch.cs

---
+++ SalesByMatch.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar)
+    {
+        Array.Sort(ar);
+        int result = 0;
+        for (int i = 0; i < ar.Length; i++)
+        {
+            int number = ar[i];
+            if (i == ar.Length - 1)
+            {
+                break;
+            }
+
+            int nextNumber = ar[i + 1];
+            if (number == nextNumber)
+            {
+                result++;
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
